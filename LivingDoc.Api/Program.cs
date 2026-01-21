97a858b28ef04243a2eea9416cce4c74e7e061c8
bmenezes05 <bmenezes05@gmail.com>
2026-01-21 13:52:22 -0300

    fix swagger

--- LivingDoc.Api/Program.cs
+++ LivingDoc.Api/Program.cs
@@ -7,11 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 var summaries = new[]
 {
